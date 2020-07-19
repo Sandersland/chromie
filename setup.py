6d80f1bd2894821303bcc7fc0410fa76af6fb024
Steffen Andersland <stefandersland@gmail.com>
2020-07-18 22:49:41 -0500

    incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="chromie",
-    version="0.1.2",
+    version="0.1.3",
     long_description=long_description,
     packages=find_packages(),
     long_description_content_type="text/markdown",
