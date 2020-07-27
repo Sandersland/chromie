83e414cff7cdce6f0cd55b3b5fb227cffa1efcc6
Steffen Andersland <stefandersland@gmail.com>
2020-07-26 19:29:03 -0500

    added another test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,14 +1,15 @@
 import unittest
 from unittest.mock import patch, Mock
 
-from chromie.commands.init import init, OVERWRIGHT_PROMPT
+from chromie.commands.init import init, OVERWRIGHT_PROMPT, NAME_PROMPT
 from chromie.utils import ChromiePathFinder
 from tests.utils import ParserHelper
 
 
 class TestInit(unittest.TestCase):
     @patch("chromie.commands.init.make_extension_dir")
-    def test_init_makes_directory(self, mocked_make_extension_dir):
+    @patch("chromie.commands.init.ChromiePathFinder.exists", return_value=False)
+    def test_init_makes_directory(self, mocked_exists, mocked_make_extension_dir):
 
         args = ParserHelper.get_mocked_args("chromie init . -n testy -o")
         finder = ChromiePathFinder(args.filepath, args.name)
@@ -22,6 +23,12 @@ class TestInit(unittest.TestCase):
         args = ParserHelper.get_mocked_args("chromie init . -n testy")
         init(args)
         mocked_input.assert_called_with(OVERWRIGHT_PROMPT)
+
+    @patch("builtins.input", return_value="testy")
+    def test_init_prompts_for_name(self, mocked_input):
+        args = ParserHelper.get_mocked_args("chromie init .")
+        init(args)
+        mocked_input.assert_called_with(NAME_PROMPT)
 
 
 if __name__ == "__main__":
