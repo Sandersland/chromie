25772b5b66a4c6b01677bd03e3af1e030f8ab57b
Steffen Andersland <stefandersland@gmail.com>
2020-08-22 13:21:23 -0400

    attempting to fix whatever is broken

--- chromie/__version__.py
+++ chromie/__version__.py
@@ -1,6 +1,8 @@
 __title__ = "chromie"
-__description__ = "Aiding in chrome extension development."
+__description__ = (
+    "A simple utility CLI tool for packaging and maintaining chrome extensions."
+)
 __author__ = "Steffen Andersland"
-__version__ = "0.1.12"
+__version__ = "0.1.14"
 __license__ = "MIT"
 __email__ = "steffen@andersland.dev"
