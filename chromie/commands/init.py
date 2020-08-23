ea8af1d8434e1844b8d27cf87f07e36abdb60ec9
Steffen Andersland <stefandersland@gmail.com>
2020-08-23 17:51:23 -0400

    WIP, updated init again and added more tests

--- chromie/commands/init.py
+++ chromie/commands/init.py
@@ -1,28 +1,27 @@
 import shutil
-import pathlib
 import os
 
 from chromie.utils import ChromiePathFinder, ManifestFile
 from chromie.enum import Initialize, Path
 
 
-def make_extension_dir(finder):
-    print("The following files and directories were created.")
-    for d in [Path.DIST_DIR, Path.SRC_DIR, Path.STORE_DIR, Path.IMAGES_DIR]:
-        p = pathlib.Path(os.path.join(finder.root, d))
-        print(p.absolute())
-        p.mkdir(parents=True)
-
-    with open(finder(Path.IGNORE_FILE), "w") as f:
+def make_ignore_file(root):
+    with open(os.path.join(root, Path.IGNORE_FILE), "w") as f:
         f.write("")
 
+
+def make_extension_dir(root, name):
+    absroot = os.path.abspath(root)
+    os.makedirs(os.path.join(absroot, Path.STORE_DIR))
+    os.makedirs(os.path.join(absroot, Path.IMAGES_DIR))
+
+    make_ignore_file(absroot)
+
     manifest = ManifestFile(
-        os.path.join(finder.root, Path.MANIFEST_FILE),
-        {"name": finder.name, "manifest_version": 2, "version": "0.0.0"},
+        os.path.join(absroot, Path.MANIFEST_FILE),
+        {"name": name, "manifest_version": 2, "version": "0.0.0"},
     )
-    print(manifest.path)
     manifest.write()
-    
 
 
 def init(args):
@@ -52,4 +51,4 @@ def init(args):
         shutil.rmtree(finder.root, ignore_errors=True)
 
     if not finder.exists(finder.root):
-        make_extension_dir(finder)
+        make_extension_dir(finder.root, finder.name)
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,13 +1,45 @@
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, call, mock_open
+import os
 
-from chromie.commands.init import init
-from chromie.enum import Initialize
+from chromie.commands.init import *
+from chromie.enum import Initialize, Path
 from chromie.utils import ChromiePathFinder
 from tests.utils import ParserHelper
 
 
 class TestInit(unittest.TestCase):
+    def test_make_ignore_file(self):
+        args = ParserHelper.get_mocked_args("chromie init -n testy -o")
+        finder = ChromiePathFinder(args.filepath, args.name)
+        absroot = os.path.abspath(finder.root)
+        m = mock_open()
+        with patch("chromie.commands.init.open", m, create=True):
+            make_ignore_file(absroot)
+            m.assert_called_once_with(os.path.join(absroot, Path.IGNORE_FILE), "w")
+            handle = m()
+            handle.write.assert_called_once_with("")
+
+    @patch("chromie.commands.init.make_ignore_file")
+    @patch("chromie.commands.init.ManifestFile.write")
+    @patch("chromie.commands.init.os.makedirs")
+    def test_make_extension_dir(
+        self, mocked_makedirs, mocked_manifest_write, mocked_make_ignore
+    ):
+        args = ParserHelper.get_mocked_args("chromie init -n testy -o")
+        finder = ChromiePathFinder(args.filepath, args.name)
+
+        make_extension_dir(finder.root, finder.name)
+        absroot = os.path.abspath(finder.root)
+        self.assertEqual(mocked_makedirs.call_count, 2)
+        mocked_makedirs.assert_has_calls(
+            [
+                call(os.path.join(absroot, Path.STORE_DIR)),
+                call(os.path.join(absroot, Path.IMAGES_DIR)),
+            ],
+        )
+        mocked_make_ignore.assert_called_with(os.path.abspath(finder.root))
+
     @patch("chromie.commands.init.make_extension_dir")
     @patch("chromie.commands.init.ChromiePathFinder.exists", return_value=False)
     def test_init_makes_directory(self, mocked_exists, mocked_make_extension_dir):
@@ -16,7 +48,7 @@ class TestInit(unittest.TestCase):
         finder = ChromiePathFinder(args.filepath, args.name)
         init(args)
 
-        mocked_make_extension_dir.assert_called_with(finder)
+        mocked_make_extension_dir.assert_called_with(finder.root, finder.name)
 
     @patch("chromie.commands.init.ChromiePathFinder.exists", return_value=True)
     @patch("builtins.input", return_value="y")
