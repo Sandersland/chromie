c391e1ca44e79b5dbea98727a682d06071b3115f
Steffen Andersland <stefandersland@gmail.com>
2020-08-25 22:11:22 -0400

    implemented a test for config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,17 +1,23 @@
+import json
+import tempfile
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
-from chromie import config
+from chromie.config import Config
 
 
 class TestConfig(unittest.TestCase):
-    def test_from_file(self):
-        pass
+    @patch("chromie.config.os.path.isfile", return_value=True)
+    def test_from_file_exists(self, mock_isfile):
+        data = '{"hello":"there"}'
+        m = mock_open(read_data=data)
+        path = "./this/is/a/test"
+        with patch("chromie.config.open", m):
+            result = Config.from_file(path)
+            m.assert_called_once_with(path, "r")
+            self.assertEqual(result.data, json.loads(data))
 
-    def test_set_values(self):
-        pass
-
-    def test_file_does_not_exist(self):
+    def test_from_file_does_not_exist(self):
         pass
 
 
