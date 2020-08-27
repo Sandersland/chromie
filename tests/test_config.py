12da23efd753eeee8623fcc5fb98e8f886d3ab54
Steffen Andersland <stefandersland@gmail.com>
2020-08-26 21:33:16 -0400

    added test for config when file doesn't exist

--- tests/test_config.py
+++ tests/test_config.py
@@ -17,8 +17,16 @@ class TestConfig(unittest.TestCase):
             m.assert_called_once_with(path, "r")
             self.assertEqual(result.data, json.loads(data))
 
-    def test_from_file_does_not_exist(self):
-        pass
+    @patch("chromie.config.os.path.isfile", return_value=False)
+    def test_from_file_does_not_exist(self, mock_isfile):
+        m = mock_open()
+        path = "./this/is/a/test"
+        with patch("chromie.config.open", m, create=True):
+            result = Config.from_file(path)
+            m.assert_called_once_with(path, "w")
+            handle = m()
+            handle.write.assert_called_once_with("")
+            self.assertEqual(result.data, {})
 
 
 if __name__ == "__main__":
